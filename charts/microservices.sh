789ece7a27f3f65e06d83b42a42c68e2f8a00003
luis486 <luis.murcia1@u.icesi.edu.co>
2024-04-11 17:44:50 -0500

    Zipkin
    Add chart zipkin to fix the app, works without problems!
    
    The script in bash is working to!

--- charts/microservices.sh
+++ charts/microservices.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+# Te paras en esta carpeta charts y le das ./microservices.sh 
+
 # Directorio donde se encuentran los charts
-CHARTS_DIR="/ruta/a/tus/charts"
+CHARTS_DIR="/mnt/d/luism/OneDrive/Escritorio/'Noveno Semestre'/'Sistemas Distribuidos'/microservices_with_helm/charts"
 
 # Lanzar el despliegue de cada chart
 helm install auth-api "$CHARTS_DIR/auth-api"
 helm install log-message "$CHARTS_DIR/log-message"
 helm install frontend "$CHARTS_DIR/frontend"
+helm install todos-api "$CHARTS_DIR/todos-api"
+helm install users-api "$CHARTS_DIR/users-api"
+helm install redis-queue "$CHARTS_DIR/redis-queue"
+helm install zipkin "$CHARTS_DIR/zipkin"
 # Agrega más comandos helm install según sea necesario para tus otros charts
 
 echo "Despliegue de todos los charts completado"
+
+# Comando para eliminar todo de un solo 
+
+# helm ls --short | xargs -L1 helm uninstall 
